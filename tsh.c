aba7816ca903e736ade33a8ae5cfe09157fdcd93
hbh <hbhkl19@qq.com>
2025-08-09 16:57:53 +0800

    feat : implement handlers for SIGINT,SIGSTP,SIGCHILD

--- tsh.c
+++ tsh.c
@@ -165,6 +165,34 @@ int main(int argc, char **argv)
 */
 void eval(char *cmdline) 
 {
+    char *argv[MAXARGS]; /* Argument list execve() */
+    char buf[MAXLINE];   /* Holds modified command line */
+    int bg;              /* Should the job run in bg or fg? */
+    pid_t pid;           /* Process id */
+    
+    strcpy(buf, cmdline);
+    bg = parseline(buf, argv); 
+    if (argv[0] == NULL)  
+	return;   /* Ignore empty lines */
+
+    if (!builtin_cmd(argv)) { 
+        if ((pid = fork()) == 0) {   /* Child runs user job */
+            if (execve(argv[0], argv, environ) < 0) {
+                printf("%s: Command not found.\n", argv[0]);
+                exit(0);
+            }
+        }
+
+	/* Parent waits for foreground job to terminate */
+	if (!bg) {
+	    int status;
+	    if (waitpid(pid, &status, 0) < 0)
+		unix_error("waitfg: waitpid error");
+	}
+	else
+	    printf("%d %s", pid, cmdline);
+    }
+
     return;
 }
 
@@ -231,6 +259,10 @@ int parseline(const char *cmdline, char **argv)
  */
 int builtin_cmd(char **argv) 
 {
+    if (!strcmp(argv[0], "quit")) /* quit command */
+	exit(0);  
+    if (!strcmp(argv[0], "&"))    /* Ignore singleton & */
+	return 1;
     return 0;     /* not a builtin command */
 }
 
@@ -273,6 +305,12 @@ void sigchld_handler(int sig)
  */
 void sigint_handler(int sig) 
 {
+    pid_t fg_pid = fgpid(jobs);
+    if (fg_pid != 0) {
+        if (kill(-fg_pid, sig) < 0) { /* Send signal to the process group */
+            unix_error("sigint_handler: kill error");
+        }
+    }
     return;
 }
 
